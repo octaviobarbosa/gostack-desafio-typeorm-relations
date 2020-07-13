c301ccca04ba0f669d303c7744c1b347cbdfe806
Octavio Barbosa <octaviobar91@gmail.com>
2020-07-13 12:38:34 -0300

    ✨ challenge done

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -13,10 +13,25 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
+    const checkProductExists = await this.productsRepository.findByName(name);
+
+    if (checkProductExists) {
+      throw new AppError(`Product with name: '${name}' already created.`);
+    }
+
+    const customer = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    return customer;
   }
 }
 
